85a62fde59c17b5fca4d76ed8656fea9d899ecd1
stevehind <steve.hind@gmail.com>
2020-01-20 22:03:41 -0800

    Further CORS wrangling.

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ def home():
     return "This is an Easter Egg. Happy Easter. Go to https://stevehind.me"
 
 # Send a message to Steve
-@app.route("/web-sms", methods = ['GET', 'POST', 'OPTIONS'])
+@app.route("/web-sms", methods = ['GET', 'POST'])
 def sms_steve():
     custom = False
 
